0667b8e6eefa5567bdc59fbc796150f983c7335d
Mark Ulaya <markulaya2@gmail.com>
2025-04-25 13:37:13 +0200

    improve configuration
    - remove unneccessary config files
    - remove tailwind 3
    - make air delete tmp directory when process exits

--- tailwind.config.js
+++
@@ -1,8 +0,0 @@
-/** @type {import('tailwindcss').Config} */
-export default {
-    content: ["./web/**/*.{vue,js,ts,jsx,tsx}", "./web/views/root.html"],
-    theme: {
-        extend: {},
-    },
-    plugins: [],
-}
