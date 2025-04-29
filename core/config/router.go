ad2a6cb8f4dd1b201c94a125a9b553cffea21237
Mark Ulaya <markulaya2@gmail.com>
2025-04-29 16:13:13 +0200

    fix serving of build assets

--- core/config/router.go
+++ core/config/router.go
@@ -4,6 +4,7 @@ import (
 	"net/http"
 	"os"
 	"path"
+	"strings"
 
 	"github.com/pezanitech/maziko/app/routes/index"
 	inertia "github.com/romsar/gonertia"
@@ -18,24 +19,27 @@ func Router(i *inertia.Inertia) http.Handler {
 			index.POST(i, w, r)
 		case r.URL.Path == "/news" && r.Method == http.MethodGet:
 			index.GET(i, w, r)
-		case r.URL.Path == "/build/" && r.Method == http.MethodGet:
-			handleBuildDir()
+		case strings.HasPrefix(r.URL.Path, "/build/"):
+			buildDirHandler().ServeHTTP(w, r)
 		default:
-			handleFiles(w, r)
+			staticFileHandler().ServeHTTP(w, r)
 		}
 	})
 }
 
-func handleFiles(w http.ResponseWriter, r *http.Request) {
-	if _, err := os.Stat(path.Join("app/public", r.URL.Path)); err == nil {
-		http.ServeFile(w, r, path.Join("app/public", r.URL.Path))
-		return
-	}
-	// If none exist, respond with a 404 error
-	http.NotFound(w, r)
+func staticFileHandler() http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		if _, err := os.Stat(path.Join("app/public", r.URL.Path)); err == nil {
+			http.ServeFile(w, r, path.Join("app/public", r.URL.Path))
+			return
+		}
+
+		// If none exist, respond with a 404 error
+		http.NotFound(w, r)
+	})
 }
 
-func handleBuildDir() http.Handler {
+func buildDirHandler() http.Handler {
 	return http.StripPrefix(
 		"/build/",
 		http.FileServer(http.Dir("./build")),
