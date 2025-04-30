268c25f3b005ca67694a5234c52abbb80d9b803d
Mark Ulaya <markulaya2@gmail.com>
2025-04-30 14:17:15 +0200

    start cleaning up route generation

--- core/cmd/gen.go
+++ core/cmd/gen.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"os"
 	"path/filepath"
+	"strings"
 
 	"github.com/pezanitech/maziko/core/utils"
 )
@@ -17,76 +18,8 @@ var (
 	packagePrefix = "github.com/pezanitech/maziko/"
 )
 
-func registerRoutes(path string, dirEntry os.DirEntry, err error) error {
-	if err != nil {
-		return err
-	}
-
-	if dirEntry.IsDir() {
-		// don't register routes directory
-		if path == routesDir {
-			return nil
-		}
-
-		fmt.Println("Path:", path)
-
-		// Write path to routesgen.go file
-		routesgenPath := filepath.Join(genDir, "routesgen.go")
-
-		// Open file in append mode
-		f, err := os.OpenFile(routesgenPath, os.O_APPEND|os.O_WRONLY, 0644)
-		if err != nil {
-			utils.Logger.Error(
-				"Error opening routesgen.go file",
-				"error", err,
-			)
-			return err
-		}
-		defer f.Close()
-
-		// Write path to file
-		if _, err := f.WriteString(fmt.Sprintf("import \"%s%s\"\n", packagePrefix, path)); err != nil {
-			utils.Logger.Error(
-				"Error writing to routesgen.go file",
-				"error", err,
-			)
-			return err
-		}
-	}
-
-	return nil
-}
-
-func GenerateRoutes() {
-	// Initialize logger before use
-	utils.InitLogger()
-
-	// Initialize a fresh routesgen.go file before walking the routes
-	routesgenPath := filepath.Join(genDir, "routesgen.go")
-
-	utils.Logger.Info("Generating routes definitions...")
-
-	// Create directory if it doesn't exist
-	if err := os.MkdirAll(genDir, 0755); err != nil {
-		utils.Logger.Error(
-			"Error creating gen directory",
-			"error", err,
-		)
-		panic(fmt.Sprintf("Failed to create gen directory: %v", err))
-	}
-
-	// Create a new empty file (overwriting any existing one)
-	f, err := os.Create(routesgenPath)
-	if err != nil {
-		utils.Logger.Error(
-			"Error creating routesgen.go file",
-			"error", err,
-		)
-		panic(fmt.Sprintf("Failed to create routesgen.go: %v", err))
-	}
-
-	// Write package declaration
-	if _, err := f.WriteString(`
+// Package declaration and common imports
+var packageDeclaration = `
 package gen
 
 import "net/http"
@@ -94,38 +27,10 @@ import "os"
 import "path"
 import "strings"
 import inertia "github.com/romsar/gonertia"
+`
 
-`); err != nil {
-		utils.Logger.Error(
-			"Error writing package declaration to routesgen.go",
-			"error", err,
-		)
-		panic(fmt.Sprintf("Failed to write to routesgen.go: %v", err))
-	}
-	f.Close()
-
-	if err := filepath.WalkDir(routesDir, registerRoutes); err != nil {
-		utils.Logger.Error(
-			"Error registering routes",
-			"error", err,
-		)
-
-		panic(fmt.Sprintf("Failed to register routes: %v", err))
-	}
-
-	// Open file in append mode
-	f, err = os.OpenFile(routesgenPath, os.O_APPEND|os.O_WRONLY, 0644)
-	if err != nil {
-		utils.Logger.Error(
-			"Error opening routesgen.go file",
-			"error", err,
-		)
-		panic(fmt.Sprintf("Failed to open routesgen.go: %v", err))
-	}
-	defer f.Close()
-
-	// Write routing definitions
-	if _, err := f.WriteString(fmt.Sprintf(`
+// Route handler definitions
+var routeHandler = fmt.Sprintf(`
 func DefineRoutes(i *inertia.Inertia) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		switch true {
@@ -141,12 +46,14 @@ func DefineRoutes(i *inertia.Inertia) http.Handler {
 			handleRequest(w, r, staticFileHandler)
 		}
 	})
-}
-	
+}`, buildPrefix)
+
+var requestHandler = `
 func handleRequest(w http.ResponseWriter, r *http.Request, f func() http.Handler) {
 	f().ServeHTTP(w, r)
-}
+}`
 
+var staticFileHandler = fmt.Sprintf(`
 func staticFileHandler() http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		resourcePath := path.Join("%s", r.URL.Path)
@@ -160,17 +67,108 @@ func staticFileHandler() http.Handler {
 		// no file exists, respond with a 404
 		http.NotFound(w, r)
 	})
-}
+}`, publicDir)
 
+var buildDirHandler = fmt.Sprintf(`
 func buildDirHandler() http.Handler {
 	return http.StripPrefix(
 		"%s",
 		http.FileServer(http.Dir("%s")),
 	)
+}`, buildPrefix, buildDir)
+
+// collectRouteImports gathers all route imports while walking the routes directory
+func collectRouteImports(path string, dirEntry os.DirEntry, err error) (string, error) {
+	if err != nil {
+		return "", err
+	}
+
+	if dirEntry.IsDir() {
+		// don't register routes directory
+		if path == routesDir {
+			return "", nil
+		}
+
+		// Build import statement for this directory
+		return fmt.Sprintf("import \"%s%s\"\n", packagePrefix, path), nil
+	}
+
+	return "", nil
 }
-`, buildPrefix, publicDir, buildPrefix, buildDir)); err != nil {
+
+// collectAllRouteImports walks through the routes directory and collects all route imports
+func collectAllRouteImports() (string, error) {
+	var imports strings.Builder
+	err := filepath.WalkDir(routesDir, func(path string, dirEntry os.DirEntry, err error) error {
+		if err != nil {
+			return err
+		}
+
+		importStatement, err := collectRouteImports(path, dirEntry, err)
+		if err != nil {
+			return err
+		}
+
+		if importStatement != "" {
+			imports.WriteString(importStatement)
+			utils.Logger.Info("Adding route", "path", path)
+		}
+
+		return nil
+	})
+
+	return imports.String(), err
+}
+
+func GenerateRoutes() {
+	// Initialize logger before use
+	utils.InitLogger()
+
+	utils.Logger.Info("Generating routes definitions...")
+
+	// Create directory if it doesn't exist
+	if err := os.MkdirAll(genDir, 0755); err != nil {
 		utils.Logger.Error(
-			"Error writing package declaration to routesgen.go",
+			"Error creating gen directory",
+			"error", err,
+		)
+		panic(fmt.Sprintf("Failed to create gen directory: %v", err))
+	}
+
+	routesgenPath := filepath.Join(genDir, "routesgen.go")
+
+	// Build the file content in memory
+	var fileContent strings.Builder
+
+	// Write package declaration
+	fileContent.WriteString(packageDeclaration)
+
+	// Collect all route imports
+	imports, err := collectAllRouteImports()
+	if err != nil {
+		utils.Logger.Error(
+			"Error collecting route imports",
+			"error", err,
+		)
+		panic(fmt.Sprintf("Failed to collect route imports: %v", err))
+	}
+
+	// Add imports to file content
+	fileContent.WriteString(imports)
+
+	// Write routing definitions
+	fileContent.WriteString(routeHandler)
+	fileContent.WriteString(requestHandler)
+	fileContent.WriteString(staticFileHandler)
+	fileContent.WriteString(buildDirHandler)
+
+	// Log the generated content
+	utils.Logger.Info("Generated routes file content", "path", routesgenPath)
+
+	// Write the full content to the file at once
+	if err := os.WriteFile(routesgenPath, []byte(fileContent.String()), 0644); err != nil {
+		utils.Logger.Error(
+			"Error writing to routesgen.go file",
 			"error", err,
 		)
 		panic(fmt.Sprintf("Failed to write to routesgen.go: %v", err))
