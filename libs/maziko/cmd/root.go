7ed8cf4a043bd437ee340e1fdaad7f8eb3cece48
Mark Ulaya <markulaya2@gmail.com>
2025-05-08 21:51:33 +0200

    start creating maziko cli - start creating create command

---
+++ libs/maziko/cmd/root.go
@@ -0,0 +1,35 @@
+package cmd
+
+import (
+	"fmt"
+
+	"github.com/spf13/cobra"
+)
+
+// Version of the CLI
+const Version = "0.1.0"
+
+// Represents the base command when called without subcommands
+var RootCmd = &cobra.Command{
+	Use: "maziko",
+	Short: fmt.Sprintf(
+		"%s\n%s\n",
+		"Maziko - A full-stack framework",
+		"built with Inertia.js and Go",
+	),
+	Version: Version,
+	Run: func(cmd *cobra.Command, args []string) {
+		// no subcommand, print help
+		if len(args) == 0 {
+			cmd.Help()
+		}
+	},
+}
+
+func InitRootCmd() {
+	// define global flags and options.
+	RootCmd.PersistentFlags().BoolP(
+		"verbose", "v", false,
+		"Enable verbose output (not implemented)",
+	)
+}
